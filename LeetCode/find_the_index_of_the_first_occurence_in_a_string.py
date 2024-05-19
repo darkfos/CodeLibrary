1299fb92f9368cff67532d96d979106b431a3223
darkfos82 <darkfos82@gmail.com>
2024-05-19 14:47:58 +0300

    Added 2 task from leetcode

---
+++ LeetCode/find_the_index_of_the_first_occurence_in_a_string.py
@@ -0,0 +1,12 @@
+"""
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+
+class Solution:
+    #Easy
+
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.find(needle)
+        return -1
