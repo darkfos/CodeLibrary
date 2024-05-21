63b7684df1fd8ce780eb6b5e9e5a62b48f55bc0d
darkfos82 <darkfos82@gmail.com>
2024-05-21 21:33:58 +0300

    Added a csv representation of array task

---
+++ CodeWars/csv_representation_of_array.py
@@ -0,0 +1,14 @@
+"""
+    Create a function that returns the CSV representation of a two-dimensional numeric array.
+    Array's length > 2.
+
+    More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+    Note: you shouldn't escape the \n, it should work as a new line.
+"""
+
+
+def to_csv_text(array):
+    #7 kyu
+
+    array = list(map(lambda x: [str(numb) for numb in x], array))
+    return "\\n".join(list(map(lambda arr: ",".join(arr), array)))
